dc3bf50b1a69449016b9a6ebc56e80cae8844d10
Leon Thorne <ldthorne@brandeis.edu>
2015-12-14 14:27:02 -0500

    finished adding directives

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -1,4 +1,6 @@
 app.controller('MainController', function (FlashCardsFactory, $scope) {
+    
+    $scope.cardsLoading = true;
 
     $scope.categories = [
         'MongoDB',
@@ -12,16 +14,22 @@ app.controller('MainController', function (FlashCardsFactory, $scope) {
     $scope.flashCards = [];
 
     $scope.getCategoryCards = function (theCategory) {
+        $scope.cardsLoading = true;
         $scope.currentCategory = theCategory;
         FlashCardsFactory.getFlashCardsByCategory(theCategory).then(function (cards) {
             $scope.flashCards = cards;
+            $scope.cardsLoading = false;
+
         });
     };
 
     $scope.getAllCards = function () {
+        $scope.cardsLoading = true;
         $scope.currentCategory = null;
         FlashCardsFactory.getFlashCards().then(function (cards) {
             $scope.flashCards = cards;
+            $scope.cardsLoading = false;
+
         });
     };
 
